3f0ed51f70cd09f1a17a784f49ec689d2f3c3a97
Andrew <student@epicodus.com>
2015-07-14 12:29:09 -0700

    adds a flash notice

--- spec/features/questions_page_spec.rb
+++ spec/features/questions_page_spec.rb
@@ -11,9 +11,9 @@ describe "the add a question process" do
   it "will add a question" do
     visit questions_path
     click_on 'Add another set of questions'
-    fill_in 'Question 1', :with => "Testing with Quesiton 1"
+    fill_in 'Question 1', :with => "Testing with Question 1"
     fill_in 'Question 2', :with => "Testing with Question 2"
     click_on 'Create Question'
-    expect(page).to_have "Testing with Question 1"
+    expect(page).to have_content "Testing with Question 1"
   end
 end
